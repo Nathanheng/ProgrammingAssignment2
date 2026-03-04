e4ef47c4cb00b1252bad5caf3ddbda37a8a654ac
nathan <231156w@mymail.nyp.edu.sg>
2026-03-04 23:19:12 +0800

    Finished Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,50 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  cached_inverse <- NULL
+  
+  list(
+    
+    set = function(new_matrix) {
+      x <<- new_matrix
+      cached_inverse <<- NULL
+    },
+    
+    get = function() {
+      x
+    },
+    
+    setinverse = function(inv) {
+      cached_inverse <<- inv
+    },
+    
+    getinverse = function() {
+      cached_inverse
+    }
+    
+  )
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  # Check if inverse already exists
+  inverse <- x$getinverse()
+  
+  if (!is.null(inverse)) {
+    message("getting cached inverse")
+    return(inverse)
+  }
+  
+  # Otherwise compute it
+  matrix_data <- x$get()
+  inverse <- solve(matrix_data, ...)
+  
+  # Store it
+  x$setinverse(inverse)
+  
+  inverse
 }
